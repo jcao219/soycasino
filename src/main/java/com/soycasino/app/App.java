4ead3519fbaa3939eb4cecb22457022bfce65352
Jimmy Cao <jc2462@cornell.edu>
2016-04-16 16:47:40 -0400

    initial commit

---
+++ src/main/java/com/soycasino/app/App.java
@@ -0,0 +1,13 @@
+package com.soycasino.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
