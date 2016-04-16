54eb5088b949b13c19246fff73add337560cb693
Jimmy Cao <jc2462@cornell.edu>
2016-04-16 19:49:06 -0400

    Some changes.

--- src/main/java/com/soycasino/app/App.java
+++ src/main/java/com/soycasino/app/App.java
@@ -1,6 +1,17 @@
 package com.soycasino.app;
 
-import static spark.Spark.*;
+import static spark.Spark.get;
+import static spark.Spark.post;
+import static spark.Spark.staticFileLocation;
+import static spark.Spark.stop;
+
+import org.eclipse.jetty.util.log.Log;
+import org.eclipse.jetty.util.log.StdErrLog;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
 
 /**
  * Hello world!
@@ -10,12 +21,20 @@ public class App
 {
     public static void main( String[] args )
     {
+        StdErrLog log = new StdErrLog();
+        Log.setLog(log);
+        log.setLevel(StdErrLog.LEVEL_WARN);
         staticFileLocation("com/soycasino/res");
 
         get("/hello", (req, res) -> "Hello World");
+        post("/login", App::login);
         get("/exit", (req, res) -> {
             stop();
             return "";
         });
+    }
+    
+    public static String login(Request req, Response res) {
+        return req.queryParams("username");
     }
 }
